8e899728f7841740b45f9130a774ed949c45d021
F. Jerrell Schivers <jerrell@bordercore.com>
2013-06-16 21:17:56 -0400

    Add more data to the homepage

--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from quote.models import Quote
+from todo.models import Todo
+from music.models import Listen
 
 SECTION = 'Home'
 
@@ -11,6 +13,12 @@ def homepage(request):
 
     quote = Quote.objects.order_by('?')[0]
 
+    # Get the latest 'urgent' todo tasks
+    tasks = Todo.objects.filter(is_urgent=True).order_by('-modified')[:3]
+
+    # Get some recently played music
+    music = Listen.objects.all().select_related().distinct().order_by('-created')[:3]
+
     return render_to_response('homepage/index.html',
-                              {'section': SECTION, 'quote': quote },
+                              {'section': SECTION, 'quote': quote, 'tasks': tasks, 'music': music },
                               context_instance=RequestContext(request))
