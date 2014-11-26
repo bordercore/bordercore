d45f73254e21772feb7e0dd5245a04045d3c2716
F. Jerrell Schivers <jerrell@bordercore.com>
2014-11-25 19:14:03 -0500

    Only search for tags which have been applied to at least one blog post

--- tag/views.py
+++ tag/views.py
@@ -8,8 +8,13 @@ import json
 @login_required
 def tag_search(request):
 
-    # Use this for the new Twitter typeahead
-    tag_list = [{'value':x.name} for x in Tag.objects.filter(name__istartswith=request.GET.get('query', '')).distinct('name')]
+    args = {}
+
+    # Only retrieve tags which have been applied to at least one blog post
+    if request.GET.get('type') == 'blog':
+        args['post__isnull'] = False
+
+    tag_list = [{'value':x.name} for x in Tag.objects.filter(name__istartswith=request.GET.get('query', ''), **args).distinct('name')]
 
     return render_to_response('return_json.json',
                               {'info': json.dumps(tag_list)},
