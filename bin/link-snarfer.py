75cd1e47a58630a5681ee389433d2e8526c6773a
F. Jerrell Schivers <jerrell@bordercore.com>
2015-05-24 16:31:07 -0400

    Fix logging error Use html package from lxml for parsing Add custom user agent to url requests

--- bin/link-snarfer.py
+++ bin/link-snarfer.py
@@ -8,8 +8,7 @@ import pprint
 import sys
 import re
 import requests
-from lxml import etree
-from lxml.html import fromstring
+from lxml import html
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'bordercore.settings'
 sys.path.insert(0, '/home/www/htdocs/bordercore-django')
@@ -24,6 +23,10 @@ pp = pprint.PrettyPrinter()
 p = re.compile("(https?://[^\">\s\n]*)[\">\s\n]")
 ignore = re.compile("doubleclick|https://twitter.com|tapbots.com|tapbots.net|search.twitter.com")
 
+# Remove existing handlers added by Django
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M:%S',
@@ -36,18 +39,16 @@ logger = logging.getLogger('bordercore.linksnarfer')
 requests_log = logging.getLogger("requests")
 requests_log.setLevel(logging.WARNING)
 
+
 def get_link_info(link):
-    r = requests.get(link)
+    headers = {'user-agent': 'Bordercore/1.0'}
+    r = requests.get(link, headers=headers)
     http_content = r.text.encode('utf-8')
 
-    # If this link is a redirect (common for shortened urls), we
-    #  want to keep the redirected link.
-    # for prev in r.history:
-    #     print "  redirected url: %s" % prev.url
-
     # http://stackoverflow.com/questions/15830421/xml-unicode-strings-with-encoding-declaration-are-not-supported
-    parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
-    doc = fromstring(http_content, parser=parser)
+    # parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
+    # doc = fromstring(http_content, parser=parser)
+    doc = html.fromstring(http_content)
     title = doc.xpath('.//title')
     if title:
         return (r.url, title[0].text)
@@ -72,10 +73,6 @@ for link in matches:
 if link_dict:
 
     for label in link_dict.keys():
-
-        print "label: %s" % label.encode('utf-8')
-        print "link: %s" % link_dict[label]
-        logger.info(link_dict[label])
-
+        logger.info("%s - %s" % (link_dict[label], label.encode('utf-8')))
         b = Bookmark(url=link_dict[label], title=label or 'No Label', user_id=1)
         b.save()
