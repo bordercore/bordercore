ad48b7c31ec79c1ccf7c636782b85772cd29b68c
F. Jerrell Schivers <jerrell@bordercore.com>
2016-01-31 18:14:29 -0500

    Disable automatic snarfing of favicons of links on the bookmarks list page

--- site_utils.py
+++ site_utils.py
@@ -3,7 +3,8 @@ import re
 from django.http import HttpResponseNotFound, HttpResponse
 from django.conf import settings
 
-from bookmark.tasks import snarf_favicon
+# from bookmark.tasks import snarf_favicon
+
 
 def handler404(request):
 
@@ -12,10 +13,9 @@ def handler404(request):
     p = re.compile(".*/img/favicons/(.*)")
     m = p.match(url)
     if m:
-        url = m.group(1)
-        if url != 'default':
-            print "No favicon for %s" % url
-            snarf_favicon.delay(url, False)
+        # url = m.group(1)
+        # if url != 'default':
+        #     snarf_favicon.delay(url, False)
 
         # Serve back a default favicon
         with open("%s/templates/static/img/favicons/default.png" % (settings.SITE_ROOT,), "rb") as f:
