235d03b620bbd18c32bb6c3455cbdc4f48b31cc9
F. Jerrell Schivers <jerrell@bordercore.com>
2016-03-06 21:49:57 -0500

    Add first batch of data tests

---
+++ blob/test_data.py
@@ -0,0 +1,105 @@
+import django
+import json
+import os
+import solr
+import sys
+
+from django.db.models import Count
+from django.conf import settings
+# from django.test import TestCase
+from blob.models import Blob
+from tag.models import Tag
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bordercore.settings'
+# sys.path.insert(0, '/home/www/htdocs/bordercore-django')
+# sys.path.insert(0, '/home/www/htdocs/bordercore-django/bordercore')
+sys.path.insert(0, '/home/jerrell/dev/django')
+sys.path.insert(0, '/home/jerrell/dev/django/bordercore')
+
+django.setup()
+
+# t = Tag.objects.filter(name='linux')
+# print t[0].name
+
+# class DataQualityTests(TestCase):
+
+#     def setUp(self):
+#         os.environ['DJANGO_SETTINGS_MODULE'] = 'bordercore.settings'
+#         sys.path.insert(0, '/home/jerrell/dev/django')
+#         sys.path.insert(0, '/home/jerrell/dev/django/bordercore')
+#         self.conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+
+#     def test_books_with_tags(self):
+
+#         solr_args = {'q': 'doctype:book AND -tags:[* TO *]',
+#                      'fl': 'id',
+#                      'wt': 'json'}
+
+#         response = self.conn.raw_query(**solr_args)
+#         data = json.loads(response)['response']['numFound']
+#         self.assertEquals(data, 0)
+
+
+def test_books_with_tags():
+    "Assert that all books have at least one tag"
+    solr_args = {'q': 'doctype:book AND -tags:[* TO *]',
+                 'fl': 'id',
+                 'wt': 'json'}
+
+    conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+    response = conn.raw_query(**solr_args)
+    data = json.loads(response)['response']['numFound']
+    assert data == 0, "%s books fail this test" % data
+
+
+def test_books_with_title():
+    "Assert that all books have a title"
+    solr_args = {'q': 'doctype:book AND -title:[* TO *]',
+                 'fl': 'id',
+                 'wt': 'json'}
+
+    conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+    response = conn.raw_query(**solr_args)
+    data = json.loads(response)['response']['numFound']
+    assert data == 0, "%s books fail this test" % data
+
+
+def test_books_with_author():
+    "Assert that all books have at least one author"
+    solr_args = {'q': 'doctype:book AND -author:[* TO *]',
+                 'fl': 'id',
+                 'wt': 'json'}
+
+    conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+    response = conn.raw_query(**solr_args)
+    data = json.loads(response)['response']['numFound']
+    assert data == 0, "%s books fail this test" % data
+
+
+def test_tags_all_lowercase():
+    "Assert that all tags are lowercase"
+    t = Tag.objects.filter(name__regex=r'[[:upper:]]+')
+    assert len(t) == 0, "%s tags fail this test" % len(t)
+
+
+# I'm not sure I need this test, as there could be legitimate reasons this test might fail
+def test_blobs_with_duplicate_filenames():
+    "Assert that all blobs have unique filenames"
+    t = Blob.objects.values('filename').order_by().annotate(dcount=Count('sha1sum')).filter(dcount__gt=1)
+    assert len(t) == 0, "%s tags fail this test" % len(t)
+
+
+def test_blob_permissions():
+    "Alert that all blobs are owned by www-data"
+    import os
+    from os import stat
+    from pwd import getpwuid
+
+    owner = "www-data"
+    walk_dir = "/home/media/blobs/"
+
+    for root, subdirs, files in os.walk(walk_dir):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            assert getpwuid(stat(file_path).st_uid).pw_name == owner, "file not owned by %s: %s" % (owner, file_path)
+#            print "%s, %s" % (file_path, getpwuid(stat(file_path).st_uid).pw_name)
