2ff9a458127addb07bdaecfcbe13966837c25137
F. Jerrell Schivers <jerrell@bordercore.com>
2016-05-22 15:27:57 -0400

    Minor PEP8 cleanup

--- bordercore/document/forms.py
+++ bordercore/document/forms.py
@@ -6,13 +6,14 @@ from django.forms import ModelForm, Textarea, TextInput, ModelMultipleChoiceFiel
 from document.models import Document
 from tag.models import Tag
 
+
 # http://stackoverflow.com/questions/5608576/django-enter-a-list-of-values-form-error-when-rendering-a-manytomanyfield-as-a
 class ModelCommaSeparatedChoiceField(ModelMultipleChoiceField):
     widget = TextInput(attrs={'class': 'form-control'})
 
     def clean(self, value):
         if value is not None:
-            value = [item.strip().lower() for item in value.split(",") if item.strip() != ''] # remove padding and force lowercase
+            value = [item.strip().lower() for item in value.split(",") if item.strip() != '']  # remove padding and force lowercase
         # Check if any of these tags are new.  The ORM won't create them for us, and in
         # fact will complain that the tag 'is not one of the available choices.'
         # These need to be explicitly created.
