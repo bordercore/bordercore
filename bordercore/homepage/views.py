aae5e63875e6f7ec984b9294a0fc96c842f94bea
F. Jerrell Schivers <jerrell@bordercore.com>
2016-09-05 20:55:59 -0400

    Add random image to homepage

--- bordercore/homepage/views.py
+++ bordercore/homepage/views.py
@@ -1,12 +1,16 @@
 import datetime
 import json
 from PyOrgMode import OrgDataStructure
+import random
+import solr
 
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from blob.models import Blob
 from bookmark.models import Bookmark
-from django.http import JsonResponse
 from quote.models import Quote
 from music.models import Listen
 from cal.models import Calendar
@@ -50,12 +54,18 @@ def homepage(request):
     # Get some recently played music
     music = Listen.objects.all().select_related().distinct().order_by('-created')[:3]
 
+    # Choose a random image
+    random_image = get_random_blob('image/*')
+    random_image_info = {'sha1sum': random_image.sha1sum,
+                         'cover_info': Blob.get_cover_info(random_image.sha1sum, 'small', 500)}
+
     return render(request, 'homepage/index.html',
                   {'section': SECTION,
                    'quote': quote,
                    'tasks': tasks,
                    'music': music,
-                   'pinned_bookmarks': pinned_bookmarks})
+                   'pinned_bookmarks': pinned_bookmarks,
+                   'random_image_info': random_image_info})
 
 
 @login_required
@@ -65,3 +75,17 @@ def get_calendar_events(request):
     events = calendar.get_calendar_info()
 
     return JsonResponse(events, safe=False)
+
+
+def get_random_blob(content_type):
+    seed = random.randint(1, 10000)
+    conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+    solr_args = {'q': 'content_type:%s' % (content_type),
+                 'sort': 'random_%s+desc' % (seed),
+                 'wt': 'json',
+                 'fl': 'internal_id,sha1sum',
+                 'fq': '-is_private:true',
+                 'rows': 1}
+    solr_results = json.loads(conn.raw_query(**solr_args))['response']['docs'][0]
+    blob = Blob.objects.get(pk=solr_results['internal_id'])
+    return blob
