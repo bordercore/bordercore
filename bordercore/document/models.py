d3f9fb6afba2b70246601172532d55dcc47db531
F. Jerrell Schivers <jerrell@bordercore.com>
2017-02-05 17:58:24 -0500

    Index documents in Solr using Django
    Don't use Java

--- bordercore/document/models.py
+++ bordercore/document/models.py
@@ -1,8 +1,13 @@
+import solr
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.db.models.signals import post_save
 import markdown
 
+from document.tasks import index_document
 from lib.mixins import TimeStampedModel
 from tag.models import Tag
 
@@ -25,3 +30,18 @@ class Document(TimeStampedModel):
 
     def get_tags(self):
         return ", ".join([tag.name for tag in self.tags.all()])
+
+    def delete(self):
+        conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+        conn.delete(queries=['id:bordercore_document_%s' % (self.id)])
+        conn.commit()
+
+        super(Document, self).delete()
+
+
+def postSaveForDocument(**kwargs):
+    instance = kwargs.get('instance')
+    index_document.delay(instance.id)
+
+
+post_save.connect(postSaveForDocument, Document)
---
+++ bordercore/document/tasks.py
@@ -0,0 +1,31 @@
+from celery import shared_task
+import solr
+
+from django.conf import settings
+
+
+@shared_task()
+def index_document(id):
+
+    # Import Django models here rather than globally at the top to avoid circular dependencies
+    from document.models import Document
+    document = Document.objects.get(pk=id)
+
+    conn = solr.SolrConnection('http://%s:%d/%s' % (settings.SOLR_HOST, settings.SOLR_PORT, settings.SOLR_COLLECTION))
+
+    doc = dict(
+        id="bordercore_document_%s" % document.id,
+        internal_id=document.id,
+        tags=[tag.name for tag in document.tags.all()],
+        author=document.author,
+        url=document.url,
+        title=document.title,
+        attr_content=document.content,
+        subtitle=document.sub_heading,
+        source=document.source,
+        importance=document.importance,
+        last_modified=document.modified,
+        doctype='document'
+    )
+    conn.add(doc)
+    conn.commit()
