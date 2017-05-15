7e367d3e80ffed977f73444fa46e065f9b12d9a5
F. Jerrell Schivers <jerrell@bordercore.com>
2017-05-14 21:23:54 -0400

    Fix widget bug on edit song page.
    The "song source" select wasn't displaying properly.

--- bordercore/music/forms.py
+++ bordercore/music/forms.py
@@ -1,7 +1,13 @@
-from django.forms import ModelForm, Select, Textarea, TextInput
+from django.forms import ModelChoiceField, ModelForm, Select, Textarea, TextInput
 from django import forms
 
-from music.models import Song, WishList
+from music.models import Song, SongSource, WishList
+
+
+# We need to create a custom class so that we can define what get's displayed in the select widget
+class MyModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.name
 
 
 class SongForm(ModelForm):
@@ -25,6 +31,10 @@ class SongForm(ModelForm):
             raise forms.ValidationError(u'If you specify an album you must also specify the year')
         return a
 
+    source = MyModelChoiceField(queryset=SongSource.objects.all(),
+                                widget=forms.Select(attrs={'class': 'form-control'}),
+                                empty_label='Select Source')
+
     class Meta:
         model = Song
         fields = ('title', 'artist', 'track', 'year', 'comment', 'source', 'times_played', 'length', 'id')
@@ -32,7 +42,7 @@ class SongForm(ModelForm):
             'title': TextInput(attrs={'class': 'form-control'}),
             'artist': TextInput(attrs={'class': 'form-control'}),
             'comment': Textarea(attrs={'rows': 2, 'class': 'form-control'}),
-            'source': Select(attrs={'class': 'form-control'}),
+            'source': Select(),
             'track': TextInput(attrs={'class': 'form-control'}),
             'year': TextInput(attrs={'class': 'form-control'}),
             'length': TextInput(attrs={'readonly': True, 'class': 'form-control'}),
