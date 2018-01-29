972faa2d77d6c7d7e7837c84cf9ac2170def7773
F. Jerrell Schivers <jerrell@bordercore.com>
2018-01-28 20:07:47 -0500

    Add test "test_one_tag_per_user"
    Only one tag per user should be in represented by a BookmarkTagUser

--- bordercore/bookmark/tests.py
+++ bordercore/bookmark/tests.py
@@ -2,15 +2,15 @@ import django
 import json
 import os
 from solrpy.core import SolrConnection
-import sys
 
+from django.db.models import Count
 from django.conf import settings
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.prod'
 
 django.setup()
 
-from bookmark.models import Bookmark
+from bookmark.models import Bookmark, BookmarkTagUser
 
 
 def test_bookmarks_in_db_exist_in_solr():
@@ -40,3 +40,9 @@ def test_solr_bookmarks_exist_in_db():
     data = json.loads(response.decode('UTF-8'))['response']
     for result in data['docs']:
         assert Bookmark.objects.filter(id=result['internal_id']).count() == 1, "bookmark %s exists in Solr but not in database" % result['internal_id']
+
+
+def test_one_tag_per_user():
+    "Only one tag per user should be in represented by a BookmarkTagUser"
+    t = BookmarkTagUser.objects.values('tag', 'user').order_by().annotate(user_count=Count('user')).filter(user_count__gt=1)
+    assert len(t) == 0, "tags found more than once per user: {}".format(t)
