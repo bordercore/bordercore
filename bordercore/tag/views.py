bffa415f075d96558930943a096cf00d91313b58
F. Jerrell Schivers <jerrell@bordercore.com>
2018-11-09 13:13:15 -0500

    Fix tag search bug
    Tag searches were only returning results for tags associated with
    blobs, not with other types of objects like bookmarks.

--- bordercore/tag/views.py
+++ bordercore/tag/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.db.models import Q
 
 from tag.models import Tag
 
@@ -14,6 +15,12 @@ def tag_search(request):
         args['document__is_blog'] = True
 
     query = request.GET.get('query', '')
-    tag_list = [{'value': x.name, 'is_meta': x.is_meta} for x in Tag.objects.filter(document__user=request.user, name__icontains=query, **args).distinct('name')]
+    tag_list = [{'value': x.name, 'is_meta': x.is_meta} for x in
+                Tag.objects.filter(Q((Q(bookmark__user=request.user) & Q(name__icontains=query))) |
+                                   Q((Q(document__user=request.user) & Q(name__icontains=query))) |
+                                   Q((Q(collection__user=request.user) & Q(name__icontains=query))) |
+                                   Q((Q(collection__user=request.user) & Q(name__icontains=query))) |
+                                   Q((Q(song__user=request.user) & Q(name__icontains=query))) |
+                                   Q((Q(todo__user=request.user) & Q(name__icontains=query))), **args).distinct('name')]
 
     return JsonResponse(tag_list, safe=False)
