ca4451e8478653b91a37ab4ab5cdba30a0029b49
F. Jerrell Schivers <jerrell@bordercore.com>
2018-11-25 15:39:07 -0500

    Fix configuration causing multiple error emails to be sent

--- config/settings/base.py
+++ config/settings/base.py
@@ -211,7 +211,7 @@ LOGGING = {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
-            'propagate': True,
+            'propagate': False,
         },
     }
 }
