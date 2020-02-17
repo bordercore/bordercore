9e143dd9ba26558db19f5d4ab06abd77bf5daf36
F. Jerrell Schivers <jerrell@bordercore.com>
2020-02-17 16:56:38 -0500

    Searches should include the "author" field

--- bordercore/search/views.py
+++ bordercore/search/views.py
@@ -154,7 +154,7 @@ class SearchListView(ListView):
                     {
                         "multi_match": {
                             "query": search_term,
-                            "fields": ["contents", "sha1sum", "title", "uuid"],
+                            "fields": ["author", "contents", "sha1sum", "title", "uuid"],
                             "operator": boolean_type,
                         }
                     }
