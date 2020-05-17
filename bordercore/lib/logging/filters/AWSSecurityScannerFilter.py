b6ed2b476c7e77c1d24f3d0693daa40cbe1f8bac
F. Jerrell Schivers <jerrell@bordercore.com>
2020-05-17 14:43:31 -0400

    Add logger for DisallowedHost
    We do this to prevent them from generating error emails.

---
+++ bordercore/lib/logging/filters/AWSSecurityScannerFilter.py
@@ -0,0 +1,11 @@
+import json
+import logging
+
+
+class AWSSecurityScannerFilter(logging.Filter):
+    def filter(self, record):
+
+        if record.request.META.get("HTTP_USER_AGENT", None) == "AWS Security Scanner":
+            return False
+
+        return True
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -16,8 +16,23 @@ LOGGING['handlers']['bordercore'] = {
     'filename': '/var/log/django/bordercore.log'
 }
 
+LOGGING['handlers']['disallowed_host'] = {
+    'level': 'ERROR',
+    'class': 'logging.handlers.RotatingFileHandler',
+    'maxBytes': 1024 * 1024 * 10,  # 10MB
+    'backupCount': 5,
+    'formatter': 'standard',
+    'filename': '/var/log/django/disallowed_host.log',
+}
+
 LOGGING['loggers']['django.request'] = {
     'handlers': ['file', 'mail_admins'],
+    'level': 'ERROR',
+    'propagate': False,
+}
+
+LOGGING['loggers']['django.security.DisallowedHost'] = {
+    'handlers': ['disallowed_host'],
     'level': 'ERROR',
     'propagate': False,
 }
