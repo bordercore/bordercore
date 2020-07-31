e021566da8c5abf2964eca048a7c69bf5ffd4c9b
F. Jerrell Schivers <jerrell@bordercore.com>
2020-07-31 15:41:40 -0400

    Add ExerciseUser to fitness admin

--- bordercore/fitness/admin.py
+++ bordercore/fitness/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from fitness.models import Data, Exercise, Muscle, MuscleGroup
+from fitness.models import Data, Exercise, ExerciseUser, Muscle, MuscleGroup
 
 admin.site.register(Data)
 admin.site.register(Exercise)
+admin.site.register(ExerciseUser)
 admin.site.register(Muscle)
 admin.site.register(MuscleGroup)
