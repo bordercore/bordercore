fb226f25f3abeab89f6e8d03fd69329c4f3b0ab6
F. Jerrell Schivers <jerrell@bordercore.com>
2020-09-28 19:54:34 -0400

    Add webpack-bundle-analyzer plugin

--- bordercore/webpack.config.js
+++ bordercore/webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
 
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
-    entry: "./front-end/index.js",  // path to our input file
+    entry: "./front-end/index.js",
     output: {
-        filename: "index-bundle.js",  // output bundle file name
-        path: path.resolve(__dirname, "./static"),  // path to our Django static directory
+        filename: "index-bundle.js",
+        path: path.resolve(__dirname, "./static"),
     },
+    // plugins: [
+    //     new BundleAnalyzerPlugin({analyzerPort: 9999})
+    // ]
 };
