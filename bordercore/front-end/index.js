4f7ec326522ac51dd1e9eeb5a7b06e286553a67b
F. Jerrell Schivers <jerrell@bordercore.com>
2020-10-01 18:20:20 -0400

    Move all JavaScript under webpack control

--- bordercore/front-end/index.js
+++ bordercore/front-end/index.js
@@ -1,21 +1,45 @@
+import Vue from "vue";
+window.Vue = Vue;
 
-import DropdownMenu from "@innologica/vue-dropdown-menu"
+import { ToastPlugin, VBHoverPlugin } from "bootstrap-vue"
+Vue.use(ToastPlugin)
+Vue.use(VBHoverPlugin)
 
-import draggable from "vuedraggable"
+import DropdownMenu from "@innologica/vue-dropdown-menu";
+Vue.component("DropdownMenu", DropdownMenu);
 
-import Vuex from "vuex"
-window.Vuex = Vuex
-Vue.use(Vuex)
+import draggable from "vuedraggable";
+Vue.component("draggable", draggable);
 
-import VueSimpleSuggest from "vue-simple-suggest/dist/es6"
-Vue.component("vue-simple-suggest", VueSimpleSuggest)
+import Vuex from "vuex";
+window.Vuex = Vuex;
+Vue.use(Vuex);
 
-// Should we use export?
-// export { capitalize, unique, longestDistinctSubstring };
+import VueSimpleSuggest from "vue-simple-suggest";
+Vue.component("vue-simple-suggest", VueSimpleSuggest);
 
-// export default {
-//     components: {
-//         Vuex,
-//         // DropdownMenu
-//     }
-// }
+import VueTagsInput from "@johmun/vue-tags-input";
+Vue.component("vue-tags-input", VueTagsInput);
+
+import Datepicker from "vuejs-datepicker";
+Vue.component("vuejs-datepicker", Datepicker);
+window.Datepicker = Datepicker;
+
+import axios from "axios";
+window.axios = axios;
+
+import $ from "jquery";
+import jQuery from "jquery";
+window.$ = $;
+window.jQuery= jQuery;
+
+import "bootstrap";
+
+import "@fortawesome/fontawesome-free/js/fontawesome"
+import "@fortawesome/fontawesome-free/js/solid"
+
+import { format } from "date-fns";
+window.format = format;
+
+import EasyMDE from "easymde";
+window.EasyMDE = EasyMDE;
--- bordercore/webpack.config.js
+++ bordercore/webpack.config.js
@@ -1,14 +1,28 @@
 const path = require("path");
+// const webpack = require("webpack")
 
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
+config = {
     entry: "./front-end/index.js",
     output: {
         filename: "index-bundle.js",
         path: path.resolve(__dirname, "./static"),
     },
+    mode: "production",
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.esm.js"
+        }
+    },
     // plugins: [
     //     new BundleAnalyzerPlugin({analyzerPort: 9999})
     // ]
 };
+
+module.exports = (env, argv) => {
+    if (argv.mode == "development") {
+        config.output.filename = "index-bundle-dev.js";
+    }
+    return config;
+}
