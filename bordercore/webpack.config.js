b42020c43952d5a7e0efe89dbe57ddf811427e21
F. Jerrell Schivers <jerrell@bordercore.com>
2020-10-19 15:18:54 -0400

    Let webpack handle SASS

--- bordercore/webpack.config.js
+++ bordercore/webpack.config.js
@@ -1,29 +1,66 @@
 const path = require("path");
-// const webpack = require("webpack")
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const CompressionPlugin = require('compression-webpack-plugin');
-
-config = {
-    entry: "./front-end/index.js",
-    output: {
-        filename: "index-bundle.js",
-        path: path.resolve(__dirname, "./static"),
-    },
-    mode: "production",
-    resolve: {
-        alias: {
-            vue$: "vue/dist/vue.esm.js"
-        }
-    },
-    plugins: [
-        // new BundleAnalyzerPlugin({analyzerPort: 9999}),
-        new CompressionPlugin()
-    ]
-};
+const CompressionPlugin = require("compression-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
 module.exports = (env, argv) => {
-    if (argv.mode == "development") {
-        config.output.filename = "index-bundle-dev.js";
+
+    let devMode = argv.mode == "development";
+
+    config = {
+        entry: {
+            "js/javascript": ["./front-end/index.js"],
+            "css/theme-light": ["./static/css/theme-light.scss"],
+            "css/theme-dark": ["./static/css/theme-dark.scss"],
+        },
+        output: {
+            filename: "[name]-bundle.min.js",
+            path: path.resolve(__dirname, "./static"),
+        },
+        mode: "production",
+        resolve: {
+            alias: {
+                vue$: "vue/dist/vue.esm.js"
+            }
+        },
+        plugins: [
+            // new BundleAnalyzerPlugin({analyzerPort: 9999}),
+
+            // Remove the boilerplate JS files from chunks of CSS only entries
+            new FixStyleOnlyEntriesPlugin(),
+
+            // Compress the JavaScript bundles
+            new CompressionPlugin({
+                test: /\.js$/i,
+                deleteOriginalAssets: true
+            }),
+
+            // Extract generated CSS into separate files
+            new MiniCssExtractPlugin({
+                filename: devMode ? "[name].css" : "[name].min.css",
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                }
+            ]
+        }
+    };
+
+    if (devMode) {
+        config.output.filename = "[name]-bundle.js";
     }
+
     return config;
+
 }
