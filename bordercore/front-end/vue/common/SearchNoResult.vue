ea8106806fb434c26a94c8340c2e63a524019c80
F. Jerrell Schivers <jerrell@bordercore.com>
2021-08-02 14:59:57 -0400

    CSS changes to SearchNoResult Vue component.

--- bordercore/front-end/vue/common/SearchNoResult.vue
+++ bordercore/front-end/vue/common/SearchNoResult.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="notice-big alert alert-info">
+    <div class="notice-big alert alert-info d-flex">
         <h3>
             <font-awesome-icon icon="search" />
-            <span class="ml-2">{{ title }}</span>
         </h3>
-        <div class="message">
+        <div class="message ml-3">
+            <h3>
+                <span>{{ title }}</span>
+            </h3>
             <slot name="message" />
         </div>
     </div>
