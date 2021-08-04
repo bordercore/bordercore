a9605ba9578c03ba7a38b0c07a58baa07a47113c
F. Jerrell Schivers <jerrell@bordercore.com>
2021-08-03 20:06:21 -0400

    Minor bug fix for DropDownMenu Vue component
    This prevents JavaScript errors when links have no click handler

--- bordercore/front-end/vue/common/DropDownMenu.vue
+++ bordercore/front-end/vue/common/DropDownMenu.vue
@@ -3,7 +3,7 @@
         <dropdown-menu v-model="showMenu" transition="translate-fade-down" class="text-center" :right="right">
             <font-awesome-icon icon="ellipsis-v" />
             <div slot="dropdown">
-                <a v-for="link in links" :key="link.id" :href="link.url" class="dropdown-item" @click="link.clickHandler">{{ link.title }}</a>
+                <a v-for="link in links" :key="link.id" :href="link.url" class="dropdown-item" v-on="link.clickHandler ? { click: link.clickHandler } : {}">{{ link.title }}</a>
             </div>
         </dropdown-menu>
     </div>
@@ -27,11 +27,6 @@
                 showMenu: false,
                 hide: true,
             };
-        },
-        methods: {
-            foobar(hovered) {
-                this.hide = !hovered;
-            },
         },
     };
 
