6cd8bc65319f353d9205edfe5e578fb566868759
F. Jerrell Schivers <jerrell@bordercore.com>
2021-08-09 20:03:42 -0400

    Expand scope of DropDownMenu

--- bordercore/front-end/vue/common/DropDownMenu.vue
+++ bordercore/front-end/vue/common/DropDownMenu.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="dropdownmenu" :class="[hide ? 'hidden' : '']">
+    <div :class="[hide ? 'hidden' : '']" class="cursor-pointer">
         <dropdown-menu v-model="showMenu" transition="translate-fade-down" class="text-center" :right="right">
-            <font-awesome-icon icon="ellipsis-v" />
+            <slot name="top">
+                <font-awesome-icon icon="ellipsis-v" />
+            </slot>
             <div slot="dropdown">
-                <a v-for="link in links" :key="link.id" :href="link.url" class="dropdown-item" v-on="link.clickHandler ? { click: link.clickHandler } : {}">{{ link.title }}</a>
+                <a v-for="link in links" :key="link.id" :href="link.url" class="dropdown-item" v-on="link.clickHandler ? { click: link.clickHandler } : {}">
+                    <font-awesome-icon v-if="link.icon" :icon="link.icon" class="mr-2" />
+                    {{ link.title }}
+                    <span v-if="link.extra" class="dropdown-item-extra">
+                        {{ link.extra }}
+                    </span>
+                </a>
             </div>
         </dropdown-menu>
     </div>
@@ -21,11 +29,15 @@
                 type: Boolean,
                 default: true,
             },
+            initialHide: {
+                type: Boolean,
+                default: true,
+            },
         },
         data() {
             return {
                 showMenu: false,
-                hide: true,
+                hide: this.initialHide
             };
         },
     };
