aa76cd17510ae3eeb48c7bd782cc103b265e2308
F. Jerrell Schivers <jerrell@bordercore.com>
2021-08-20 18:22:38 -0400

    Add update-elasticsearch-field.py management command

---
+++ bordercore/blob/management/commands/update-elasticsearch-field.py
@@ -0,0 +1,81 @@
+# Update a blob's field in Elasticsearch
+
+import sys
+
+from elasticsearch import Elasticsearch
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db.transaction import atomic
+
+es = Elasticsearch(
+    [settings.ELASTICSEARCH_ENDPOINT],
+    timeout=120,
+    verify_certs=False
+)
+
+
+class Command(BaseCommand):
+    help = "Update a blob's field in Elasticsearch"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--uuid",
+            required=True,
+            help="The uuid of the blob to update",
+        )
+        parser.add_argument(
+            "--field",
+            required=True,
+            help="The field to update",
+        )
+        parser.add_argument(
+            "--value",
+            required=True,
+            help="The new field value",
+        )
+
+    @atomic
+    def handle(self, *args, uuid, field, value, **kwargs):
+
+        self.es = Elasticsearch([settings.ELASTICSEARCH_ENDPOINT], timeout=120, verify_certs=False)
+
+        content_type_old = self.get_current_value(uuid, field)
+        self.stdout.write(f"Current content type: {content_type_old}")
+
+        if content_type_old == value:
+            self.stderr.write("Error: The blob already has that field value set.")
+            sys.exit(1)
+
+        self.set_content_type(uuid, field, value)
+
+    def get_current_value(self, uuid, field):
+
+        search_object = {
+            "query": {
+                "term": {
+                    "uuid": uuid,
+                }
+            },
+            "from": 0,
+            "size": 1,
+            "_source": [field]
+        }
+
+        return self.es.search(index=settings.ELASTICSEARCH_INDEX, body=search_object)["hits"]["hits"][0]["_source"][field]
+
+    def set_content_type(self, uuid, field, value):
+
+        request_body = {
+            "query": {
+                "term": {
+                    "uuid": uuid
+                }
+            },
+            "script": {
+                "source": f"ctx._source.{field}='{value}'",
+                "lang": "painless"
+            }
+        }
+
+        return self.es.update_by_query(index=settings.ELASTICSEARCH_INDEX, body=request_body)
