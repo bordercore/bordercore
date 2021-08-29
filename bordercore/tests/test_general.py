56803c631d34c35cd4e9fd373b7f50f32cd692df
F. Jerrell Schivers <jerrell@bordercore.com>
2021-08-29 17:28:49 -0400

    Add more HTML tests

--- bordercore/tests/test_general.py
+++ bordercore/tests/test_general.py
@@ -11,9 +11,10 @@ pytestmark = pytest.mark.data_quality
 django.setup()
 
 
-def test_no_inline_styles():
+def test_html():
     """
-    Test that there are no inline CSS styles in any template
+    Look for HTML that violates best practices, such as inline styles
+    and temporary attributes.
     """
 
     # Collect all the templates used by Django
@@ -30,7 +31,7 @@ def test_no_inline_styles():
 
         soup = BeautifulSoup(page, "lxml")
 
-        # Look for any tag with a "style" attribute
+        # Look for any inline CSS styles, ie a tag with a "style" attribute
         count = soup.find_all(attrs={"style": True})
 
         # If there is only one violation and it occurs in the "body"
@@ -41,3 +42,13 @@ def test_no_inline_styles():
             continue
 
         assert len(count) == 0, f"Found inline CSS style in template {template}"
+
+        # Look for any tag with a "_class" attribute
+        count = soup.find_all(attrs={"_class": True})
+
+        assert len(count) == 0, f"Found a tag with attribute name '_class' in template {template}"
+
+        # Look for any classes with an underscore in the name
+        count = soup.select("div[class*='_']")
+
+        assert len(count) == 0, f"Found a class name starting with '_' in template {template}"
