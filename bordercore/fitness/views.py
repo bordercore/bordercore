e239563215f9870cc668a80e8e8a0e6428bf936d
F. Jerrell Schivers <jerrell@bordercore.com>
2021-12-09 16:40:37 -0500

    Fix fitness summary display bug for new exercises

--- bordercore/fitness/views.py
+++ bordercore/fitness/views.py
@@ -109,8 +109,8 @@ def fitness_summary(request):
             # To determine when an exercise is overdue, convert the current datetime and the
             #  exercise's last active datetime to days since the epoch, then add one. If
             #  that exceeds the exercises's interval, it's overdue.
-            if e.interval and (timezone.now() - datetime.datetime(1970,1,1).astimezone()).days - \
-               (e.last_active - datetime.datetime(1970,1,1).astimezone()).days + 1 > e.interval.days:
+            if e.interval and (timezone.now() - datetime.datetime(1970, 1, 1).astimezone()).days - \
+               (e.last_active - datetime.datetime(1970, 1, 1).astimezone()).days + 1 > e.interval.days:
                 e.overdue = 1
             else:
                 e.overdue = 0
