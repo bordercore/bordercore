90ddac9c0775f9abdc77250714c6b19baadc03b3
F. Jerrell Schivers <jerrell@bordercore.com>
2021-12-11 19:55:56 -0500

    Add ability to pass in a function to AddButton Vue component

--- bordercore/front-end/vue/common/AddButton.vue
+++ bordercore/front-end/vue/common/AddButton.vue
@@ -1,7 +1,20 @@
 <template>
-    <a>
+    <a @click="clickHandler">
         <div class="button-add">
             <font-awesome-icon icon="plus" class="text-emphasis" />
         </div>
     </a>
 </template>
+
+<script>
+
+    export default {
+        props: {
+            clickHandler: {
+                default: () => {},
+                type: Function,
+            },
+        },
+    };
+
+</script>
