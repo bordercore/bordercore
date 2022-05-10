ea81460bc835e598fcb8e816877f5d58abfb2672
F. Jerrell Schivers <jerrell@bordercore.com>
2022-05-10 19:20:22 -0400

    Add collection form test

---
+++ bordercore/collection/tests/test_forms.py
@@ -0,0 +1,53 @@
+from unittest.mock import Mock
+
+from faker import Factory as FakerFactory
+
+from collection.forms import CollectionForm
+
+faker = FakerFactory.create()
+
+
+def test_collection_form(auto_login_user):
+
+    user, _ = auto_login_user()
+
+    request_mock = Mock()
+    request_mock.user = user
+
+    name = faker.text(max_nb_chars=32)
+    description = faker.text(max_nb_chars=100)
+    tags = "django,linux"
+
+    data = {
+        "description": description,
+        "tags": tags
+    }
+
+    form = CollectionForm(
+        data=data,
+        request=request_mock
+    )
+
+    # The form is missing the required field 'name'
+    assert not form.is_valid()
+
+    # After adding the required field, the form should be valid
+    data["name"] = name
+    form = CollectionForm(
+        data=data,
+        request=request_mock
+    )
+
+    assert form.is_valid()
+
+    object = form.save(commit=False)
+    object.user = user
+    object.save()
+    form.save_m2m()
+
+    assert object.tags.count() == 2
+    assert "django" in [x.name for x in object.tags.all()]
+    assert "linux" in [x.name for x in object.tags.all()]
+
+    assert object.name == name
+    assert object.description == description
