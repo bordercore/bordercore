7322f4a0bdb316ba98b92be573860f2bd4f46a73
F. Jerrell Schivers <jerrell@bordercore.com>
2022-06-01 20:56:45 -0400

    Show collection count on node list page

--- bordercore/node/services.py
+++ bordercore/node/services.py
@@ -8,10 +8,21 @@ def get_node_list(user):
     Get a list of all nodes, along with their blob and bookmark counts
     """
 
-    nodes = Node.objects.filter(user=user) \
-                        .annotate(
-                            todo_count=Count("todos")
-                        ) \
-                .order_by("-modified")
+    nodes = Node.objects.filter(
+        user=user
+    ).annotate(
+        todo_count=Count("todos")
+    ).order_by(
+        "-modified"
+    )
+
+    for node in nodes:
+        node.collection_count = len([
+            True
+            for sublist in node.layout
+            for val in sublist
+            if "uuid" in val
+            and val["type"] == "collection"
+        ])
 
     return nodes
