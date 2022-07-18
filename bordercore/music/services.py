e0369d3e4304132926e4003868d67e78e55c032d
F. Jerrell Schivers <jerrell@bordercore.com>
2022-07-18 18:30:10 -0400

    Fix playlist count bug

--- bordercore/music/services.py
+++ bordercore/music/services.py
@@ -16,22 +16,11 @@ SEARCH_LIMIT = 1000
 
 def get_playlist_counts(user):
 
-    # First get the song counts for all "smart" playlists from the "size" field
-    playlist_sizes = {
-        x["uuid"]: x["size"]
-        for x
-        in Playlist.objects.filter(user=user).values()
-        if x["size"]
-    }
-
-    # Then get the song counts for all "manual" playlists from the "PlaylistItem" table
-    playlists = Playlist.objects.filter(user=user).annotate(num_songs=Count("playlistitem"))
-
-    # Finally, combine the two
-    for p in playlists:
-        if p.type != "manual":
-            p.num_songs = playlist_sizes[p.uuid]
-    return playlists
+    return Playlist.objects.filter(
+        user=user
+    ).annotate(
+        num_songs=Count("playlistitem")
+    )
 
 
 def get_playlist_songs(playlist):
