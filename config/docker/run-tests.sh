da3bab6a6c1bf343296d0c9e369075c7fb2b6041
F. Jerrell Schivers <jerrell@bordercore.com>
2022-09-24 16:01:49 -0400

    Running tests in Docker now performs a hard git reset

--- config/docker/run-tests.sh
+++ config/docker/run-tests.sh
@@ -1,5 +1,9 @@
 # Pull the latest repo changes and run all unit and functional tests
 
 cd /bordercore
-git pull
+
+# Hard reset the HEAD, in case upstream history has changed
+git fetch --all
+git reset --hard origin/master
+
 make test_unit test_functional
