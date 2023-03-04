f64b4f4dfb4d9398ac54eff56cde5afb48b54e4f
F. Jerrell Schivers <jerrell@bordercore.com>
2023-03-03 19:33:38 -0500

    Fix top search UI issues

--- bordercore/front-end/vue/search/TopSearch.vue
+++ bordercore/front-end/vue/search/TopSearch.vue
@@ -4,19 +4,17 @@
             <form class="form-inline" method="get">
                 <input type="hidden" name="doctype" :value="searchFilter">
                 <div class="form-row">
-                    <div class="has-search me-1">
-                        <font-awesome-icon icon="search" />
-
+                    <div class="me-1">
                         <select-value
+                            id="topSearchValue"
                             ref="selectValue"
                             label="name"
-                            name="search"
                             place-holder="Search"
                             :search-url="suggestSearchUrl"
+                            @close="onClose"
+                            @keydown="onKeyDown"
                             @search-change="onSearchChange"
                             @select="select"
-                            @enter="onEnter"
-                            @keydown.prevent="onKeyDown"
                         >
                             <template #option="props">
                                 <!-- @click.stop="" handlers are needed to prevent the splitter from being selected -->
@@ -33,7 +31,7 @@
                                     <span v-if="props.option.doctype">
                                         <em class="top-search-object-type">{{ props.option.doctype }}</em> -
                                     </span>
-                                    <span class="d-inline" v-html="boldenSuggestion(props)" />
+                                    <span class="d-inline" v-html="boldenOption(props.option.name, props.search)" />
                                 </div>
                             </template>
                         </select-value>
@@ -53,7 +51,7 @@
                     Filter Options
                 </div>
                 <div class="d-flex flex-column">
-                    <div v-for="filter in searchFilterTypes" :key="filter.icon" class="search-suggestion d-flex" :class="getFilterClass(filter.doctype)" @click.prevent="handleFilter(filter.doctype)">
+                    <div v-for="filter in searchFilterTypes" :key="filter.icon" class="search-suggestion d-flex" :class="{'selected rounded-sm': filter.doctype === searchFilter}" @click.prevent="handleFilter(filter.doctype)">
                         <div class="top-search-filter-icon d-flex justify-content-center align-items-center">
                             <font-awesome-icon class="me-2" :icon="filter.icon" />
                         </div>
@@ -83,6 +81,7 @@
 <script>
 
     import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+    import {boldenOption} from "/front-end/util.js";
     import SelectValue from "../common/SelectValue.vue";
 
     export default {
@@ -121,21 +120,121 @@
             },
         },
         setup(props) {
+            const showFilter = ref(true);
+            const showSearchWindow = ref(false);
+            const searchFilter = ref(props.initialSearchFilter);
+            const selectValue = ref(null);
+
+            const searchFilterTypes = ref([
+                {
+                    "name": "Books",
+                    "icon": "book",
+                    "doctype": "book",
+                },
+                {
+                    "name": "Bookmarks",
+                    "icon": "bookmark",
+                    "doctype": "bookmark",
+                },
+                {
+                    "name": "Notes",
+                    "icon": "sticky-note",
+                    "doctype": "note",
+                },
+                {
+                    "name": "Music",
+                    "icon": "music",
+                    "doctype": "music",
+                },
+                {
+                    "name": "Drill Questions",
+                    "icon": "graduation-cap",
+                    "doctype": "drill",
+                },
+            ]);
+
             function focusSearch() {
-                selectValue.value.$el.querySelector("input").focus();
+                selectValue.value.focus();
             }
 
+            function getFilterName(doctype) {
+                const filter = searchFilterTypes.value.filter((x) => {
+                    return x.doctype === doctype;
+                });
+                return filter.length > 0 ? filter[0].name : "";
+            };
+
             function handleFilter(filter) {
+                console.log("handleFilter");
+                // showSearchWindow.value = true;
                 searchFilter.value = searchFilter.value === filter ? "" : filter;
                 saveSearchFilter(searchFilter);
             }
+
+            function handleRecentSearch(searchTerm) {
+                window.location=props.querySearchUrl + "?search=" + searchTerm.search_text;
+            };
+
+            function onClose(evt) {
+                showSearchWindow.value = false;
+            };
+
+            /* function onEnter(evt) {
+             *     console.log("onEnter");
+             *     const inputValue = document.getElementById("top-simple-suggest").value;
+             *     if (inputValue === "") {
+             *         return;
+             *     }
+             *     const form = document.querySelector("#top-search form");
+             *     if (searchFilter.value === "note") {
+             *         form.action = props.noteQuerySearchUrl;
+             *     } else if (searchFilter.value === "bookmark") {
+             *         form.action = props.bookmarkQuerySearchUrl;
+             *     } else if (searchFilter.value === "drill") {
+             *         form.action = props.drillQuerySearchUrl;
+             *     } else {
+             *         form.action = props.querySearchUrl;
+             *     }
+
+             *     form.submit();
+             * }; */
+
+            function onKeyDown(evt) {
+                if (evt.code === "KeyN" && evt.altKey) {
+                    handleFilter("note");
+                } else if (evt.code === "KeyL" && evt.altKey) {
+                    handleFilter("bookmark");
+                } else if (evt.code === "KeyB" && evt.altKey) {
+                    handleFilter("book");
+                } else if (evt.code === "KeyM" && evt.altKey) {
+                    handleFilter("music");
+                    // Hack to prevent Chrome on OS X from submitting the form.
+                    // I have no idea why this happens.
+                    evt.preventDefault();
+                } else if (evt.code === "KeyD" && evt.altKey) {
+                    handleFilter("drill");
+                } else if (evt.key === "a" && evt.altKey) {
+                    document.getElementById("top-simple-suggest").select();
+                } else if (evt.code === "Escape") {
+                    showSearchWindow.value = false;
+                }
+            };
+
+            function onSearchChange(query) {
+                showFilter.value = query === "";
+            };
+
+            function removeFilter() {
+                searchFilter.value = "";
+                handleFilter("");
+            };
 
             function saveSearchFilter(searchFilter) {
                 doPost(
                     null,
                     props.storeInSessionUrl,
                     {
-                        "top_search_filter": searchFilter,
+                        "top_search_filter": searchFilter.value,
                     }
                     ,
                     (response) => {},
@@ -144,168 +243,72 @@
                 );
             }
 
+            function select(selection) {
+                const form = document.querySelector("#top-search form");
+                if (searchFilter.value === "note") {
+                    document.getElementById("topSearchValue").value = selectValue.value.$refs.multiselect.search;
+                    form.action = props.noteQuerySearchUrl;
+                } else if (searchFilter.value === "bookmark") {
+                    form.action = props.bookmarkQuerySearchUrl;
+                } else if (searchFilter.value === "drill") {
+                    form.action = props.drillQuerySearchUrl;
+                } else {
+                    document.getElementById("topSearchValue").value = selectValue.value.$refs.multiselect.search;
+                    form.action = props.querySearchUrl;
+                }
+                form.submit();
+            };
+
             onMounted(() => {
                 document.addEventListener("keydown", function(evt) {
                     if (evt.key === "s" && evt.altKey) {
-                        self.showSearchWindow = true;
+                        showSearchWindow.value = true;
                     }
                 } );
+
+                // If a click was detected outside this component, *and*
+                //  the click wasn't on the "Search icon", then hide the
+                //  component.
+                /* document.addEventListener("click", function(event) {
+                 *     console.log(`click: ${event}`);
+                 *     const specifiedElement = document.getElementById("top-search");
+                 *     if (!specifiedElement) {
+                 *         return;
+                 *     }
+                 *     const isClickInside = specifiedElement.contains(event.target) || specifiedElement.contains(event.target.parentElement);
+                 *     // We check for the search icon by looking for a click on the
+                 *     //  font-awesome 'svg' element, which has a custom 'top-search-target'
+                 *     //  class on it, or the containing 'path' element by looking for a 'svg'
+                 *     //  parent element with the same class.
+                 *     if (!isClickInside &&
+                 *         !event.target.classList.contains("top-search-target") &&
+                 *         !event.target.parentElement.classList.contains("top-search-target") &&
+                 *         !event.target.classList.contains("fa-times") &&
+                 *         !event.target.parentElement.classList.contains("fa-times")
+                 *     ) {
+                 *         self.showSearchWindow = false;
+                 *     }
+                 * }); */
             });
 
-            const showFilter = ref(true);
-            const showSearchWindow = ref(false);
-            const searchFilter = ref(props.initialSearchFilter);
-            const selectValue = ref(null);
-
             return {
+                boldenOption,
                 focusSearch,
+                getFilterName,
                 handleFilter,
+                handleRecentSearch,
+                onClose,
+                onKeyDown,
+                onSearchChange,
+                removeFilter,
                 saveSearchFilter,
+                searchFilterTypes,
+                select,
                 selectValue,
-                searchUrl: props.suggestSearchUrl,
-                searchFilterTypes: [
-                    {
-                        "name": "Books",
-                        "icon": "book",
-                        "doctype": "book",
-                    },
-                    {
-                        "name": "Bookmarks",
-                        "icon": "bookmark",
-                        "doctype": "bookmark",
-                    },
-                    {
-                        "name": "Notes",
-                        "icon": "sticky-note",
-                        "doctype": "note",
-                    },
-                    {
-                        "name": "Music",
-                        "icon": "music",
-                        "doctype": "music",
-                    },
-                    {
-                        "name": "Drill Questions",
-                        "icon": "graduation-cap",
-                        "doctype": "drill",
-                    },
-                ],
                 showFilter,
                 showSearchWindow,
                 searchFilter,
             };
-        },
-        mounted() {
-            const self = this;
-
-            // If a click was detected outside this component, *and*
-            //  the click wasn't on the "Search icon", then hide the
-            //  component.
-            document.addEventListener("click", function(event) {
-                const specifiedElement = document.getElementById("top-search");
-                if (!specifiedElement) {
-                    return;
-                }
-                const isClickInside = specifiedElement.contains(event.target) || specifiedElement.contains(event.target.parentElement);
-                // We check for the search icon by looking for a click on the
-                //  font-awesome 'svg' element, which has a custom 'top-search-target'
-                //  class on it, or the containing 'path' element by looking for a 'svg'
-                //  parent element with the same class.
-                if (!isClickInside &&
-                    !event.target.classList.contains("top-search-target") &&
-                    !event.target.parentElement.classList.contains("top-search-target") &&
-                    !event.target.classList.contains("fa-times") &&
-                    !event.target.parentElement.classList.contains("fa-times")
-                ) {
-                    self.showSearchWindow = false;
-                }
-            });
-        },
-        methods: {
-            boldenSuggestion(scope) {
-                if (!scope) return scope;
-
-                const {option, query} = scope;
-
-                const result = option.name;
-                if (!option.doctype) {
-                    return result;
-                }
-
-                if (!query) return result;
-
-                const texts = query.split(/[\s-_/\\|\.]/gm).filter((t) => !!t) || [""];
-
-                const boldResult = result.replace(new RegExp("(.*?)(" + texts.join("|") + ")(.*?)", "gi"), "$1<b class='text-primary'>$2</b>$3");
-                return boldResult;
-            },
-            select(datum) {
-                window.location = datum.link;
-            },
-            onEnter(evt) {
-                if (this.$refs.suggestComponent.hoveredIndex != -1) {
-                    return;
-                }
-
-                const inputValue = document.getElementById("top-simple-suggest").value;
-                if (inputValue === "") {
-                    return;
-                }
-                const form = document.querySelector("#top-search form");
-
-                if (this.searchFilter === "note") {
-                    form.action = this.noteQuerySearchUrl;
-                } else if (this.searchFilter === "bookmark") {
-                    form.action = this.bookmarkQuerySearchUrl;
-                } else if (this.searchFilter === "drill") {
-                    form.action = this.drillQuerySearchUrl;
-                } else {
-                    form.action = this.querySearchUrl;
-                }
-
-                form.submit();
-            },
-            onKeyDown(evt) {
-                if (evt.code === "KeyN" && evt.altKey) {
-                    this.handleFilter("note");
-                } else if (evt.code === "KeyL" && evt.altKey) {
-                    this.handleFilter("bookmark");
-                } else if (evt.code === "KeyB" && evt.altKey) {
-                    this.handleFilter("book");
-                } else if (evt.code === "KeyM" && evt.altKey) {
-                    this.handleFilter("music");
-                    // Hack to prevent Chrome on OS X from submitting the form.
-                    // I have no idea why this happens.
-                    evt.preventDefault();
-                } else if (evt.code === "KeyD" && evt.altKey) {
-                    this.handleFilter("drill");
-                } else if (evt.key === "a" && evt.altKey) {
-                    document.getElementById("top-simple-suggest").select();
-                } else if (evt.code === "Escape") {
-                    this.showSearchWindow = false;
-                }
-            },
-            onSearchChange(query) {
-                this.showFilter = query === "";
-            },
-            handleRecentSearch(searchTerm) {
-                window.location=this.querySearchUrl + "?search=" + searchTerm.search_text;
-            },
-            getFilterClass(filter) {
-                if (filter === this.searchFilter) {
-                    return "selected rounded-sm";
-                }
-            },
-            removeFilter() {
-                this.searchFilter = "";
-                this.handleFilter("");
-            },
-            getFilterName(doctype) {
-                const filter = this.searchFilterTypes.filter((x) => {
-                    return x.doctype === doctype;
-                });
-                return filter.length > 0 ? filter[0].name : "";
-            },
         },
     };
 
