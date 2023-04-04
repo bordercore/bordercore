149f3f39c1b61b6c12684a0553d730a60a28efa5
F. Jerrell Schivers <jerrell@bordercore.com>
2023-04-04 14:49:31 -0400

    Avoid using Django "safe" template filter on question detail page

--- bordercore/drill/views.py
+++ bordercore/drill/views.py
@@ -145,7 +145,7 @@ class QuestionDetailView(DetailView):
             "tag_list": ", ".join([x.name for x in self.object.tags.all()]),
             "study_session_progress": Question.get_study_session_progress(self.request.session),
             "last_response": self.object.get_last_response(),
-            "intervals": json.dumps(self.object.get_intervals(description_only=True))
+            "intervals": self.object.get_intervals(description_only=True)
         }
 
 
