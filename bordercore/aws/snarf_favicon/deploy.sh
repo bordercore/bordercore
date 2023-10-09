3d4732f875bb165761cffe284047a7cd9fdb31a9
F. Jerrell Schivers <jerrell@bordercore.com>
2023-10-09 14:33:10 -0400

    Use Docker image to deploy SnarfFavicon lambda

--- bordercore/aws/snarf_favicon/deploy.sh
+++ bordercore/aws/snarf_favicon/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-BUCKET=bordercore-blobs
+IMAGE_REPO=192218769908.dkr.ecr.us-east-1.amazonaws.com/snarf-favicon-lambda
 TEMPLATE_FILE=packaged.yaml
 SAM=~/.local/bin/sam
 
 $SAM build --use-container &&
 
 $SAM package \
-     --s3-bucket $BUCKET \
+     --image-repository $IMAGE_REPO \
      --output-template-file packaged.yaml &&
 
 $SAM deploy \
      --template-file $TEMPLATE_FILE \
      --stack-name SnarfFaviconStack \
-     --capabilities CAPABILITY_IAM
+     --capabilities CAPABILITY_IAM \
+     --image-repository $IMAGE_REPO
--- bordercore/aws/update_feeds/deploy.sh
+++ bordercore/aws/update_feeds/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUCKET=bordercore-blobs
+IMAGE_REPO=192218769908.dkr.ecr.us-east-1.amazonaws.com/update-feeds-lambda
 TEMPLATE_FILE=packaged.yaml
 SAM=~/.local/bin/sam
 
@@ -12,11 +12,12 @@ SAM=~/.local/bin/sam
 $SAM build --use-container &&
 
 $SAM package \
-     --s3-bucket $BUCKET \
+     --image-repository $IMAGE_REPO \
      --output-template-file packaged.yaml &&
 
 $SAM deploy \
      --template-file $TEMPLATE_FILE \
      --stack-name UpdateFeedsStack \
      --capabilities CAPABILITY_IAM \
-     --parameter-overrides ParameterKey=DRFTokenParameter,ParameterValue=${DRF_TOKEN}
+     --parameter-overrides ParameterKey=DRFTokenParameter,ParameterValue=${DRF_TOKEN} \
+     --image-repository $IMAGE_REPO
