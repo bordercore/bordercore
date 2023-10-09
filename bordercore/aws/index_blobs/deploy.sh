6edea6b819ecb62f50d318afe633997166206ca4
F. Jerrell Schivers <jerrell@bordercore.com>
2023-10-09 17:48:06 -0400

    Use Docker image to deploy IndexBlob lambda

--- bordercore/aws/index_blobs/deploy.sh
+++ bordercore/aws/index_blobs/deploy.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-BUCKET=bordercore-blobs
+IMAGE_REPO=192218769908.dkr.ecr.us-east-1.amazonaws.com/index-blob-lambda
 TEMPLATE_FILE=packaged.yaml
 SAM=~/.local/bin/sam
 EFS_DIR=/mnt/efs
 BLOB_DIR=/mnt/efs/blobs
 EFS_ACCESS_POINT=fsap-034583ce1fe88d1b4
 
-# We copy the dependencies rather than use symlinks because
-#  symlinks don't work inside a "--use-container" Docker image
-cp ../../lib/util.py ./lib/
-cp ../../blob/elasticsearch_indexer.py ./lib/
+# cp ../../lib/util.py ./lib/
+# cp ../../blob/elasticsearch_indexer.py ./lib/
 
 $SAM build --use-container &&
 
 $SAM package \
-     --s3-bucket $BUCKET \
+     --image-repository $IMAGE_REPO \
      --output-template-file packaged.yaml &&
 
 $SAM deploy \
      --template-file $TEMPLATE_FILE \
      --stack-name IndexBlobStack \
      --capabilities CAPABILITY_IAM \
+     --image-repository $IMAGE_REPO \
      --parameter-overrides ParameterKey=DRFTokenParameter,ParameterValue=${DRF_TOKEN} \ ParameterKey=ElasticsearchEndpointParameter,ParameterValue=${ELASTICSEARCH_ENDPOINT} \
      ParameterKey=EFSMountPointParameter,ParameterValue=$EFS_DIR \
      ParameterKey=BlobDirParameter,ParameterValue=$BLOB_DIR \
