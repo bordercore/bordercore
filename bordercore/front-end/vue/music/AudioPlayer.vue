c2a5fe5fb23f77d3e1d49a7a460581d9f69a90ca
F. Jerrell Schivers <jerrell@bordercore.com>
2023-10-13 15:57:43 -0400

    Improve CSS of audio player

--- bordercore/front-end/vue/music/AudioPlayer.vue
+++ bordercore/front-end/vue/music/AudioPlayer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-3">
+    <div class="audio-player-wrapper mb-3 mt-3 pb-2">
         <div class="text5 text-center text-truncate mx-2">
             {{ currentTitle }}
         </div>
@@ -21,7 +21,7 @@
                 </media-control-bar>
             </media-controller>
         </div>
-        <div class="d-flex align-items-center ms-4">
+        <div class="d-flex align-items-center ms-4 mt-2">
             <o-switch id="continuous_play" v-model="continuousPlay">
                 Continous Play
             </o-switch>
